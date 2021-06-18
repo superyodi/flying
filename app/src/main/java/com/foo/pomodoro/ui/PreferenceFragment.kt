c33e1dd140e4cd858b58ca60f6a93b12a361c12c
superyodi <songagreen@gmail.com>
2021-06-18 20:49:05 +0900

    Implement #15 - 툴바 설정
    * fragment id에 따라 툴바 다르게 보여줌
    
    Close #15

--- app/src/main/java/com/foo/pomodoro/ui/PreferenceFragment.kt
+++ app/src/main/java/com/foo/pomodoro/ui/PreferenceFragment.kt
@@ -1,4 +1,17 @@
 package com.foo.pomodoro.ui
 
-class PreferenceFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PreferenceFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
