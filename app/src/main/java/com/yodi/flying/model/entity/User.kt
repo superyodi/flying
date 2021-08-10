f5da828dc4c052c7a15f8ee2c30033c0d21b2867
superyodi <songagreen@gmail.com>
2021-08-10 11:34:50 +0900

    Implement #30 - user table에 preference 관련된 속성들 추가
    * runningTime, shortBreakTime, longBreakTime, longBreakTerm, isAutoBreakMode, isAutoSkipMode, isNonBreakMode 속성 추가

--- app/src/main/java/com/yodi/flying/model/entity/User.kt
+++ app/src/main/java/com/yodi/flying/model/entity/User.kt
@@ -9,6 +9,14 @@ data class User(
     val id: Long = 0L,
     var nickname : String = ""
 ) {
+    var runningTime  = 1500000L //25min
+    var shortBreakTime = 300000L //5min
+    var longBreakTime = 900000L //15min
+    var longBreakTerm = 4
+    var isAutoBreakMode = true // 자동으로 뽀모도로 넘김
+    var isAutoSkipMode = true // 자동으로 휴식 시작
+    var isNonBreakMode = false // 휴식없이 무한 질주 
+
 
 }
 
